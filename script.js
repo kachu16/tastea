38703f6cd51679732579631ea24d9d1aff436e2d
Kajol Sharma <91527662+kachu16@users.noreply.github.com>
2023-01-01 21:26:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+const submit_button = document.getElementById("submit-btn");
+const rest_button = document.getElementById("reset-btn");
+
+submit_button.addEventListener("click", displayTea);
+rest_button.addEventListener('click',remove_help);
+
+function displayTea() {
+  const checkbox = document.getElementsByClassName("form-check-input");
+  const checkedArr = [];
+  let j = 0;
+  for (let i = 0; i < checkbox.length; i++) {
+    if (checkbox[i].checked === true) {
+      checkedArr[j] = checkbox[i].value;
+      j++;
+    }
+  }
+  const tea = document.getElementsByClassName("tea");
+
+  const suggested_tea = document.getElementById("suggested_tea");
+
+  if (checkedArr.length !==0 && document.getElementById("help_heading")===null) {
+    const heading = document.createElement("h3");
+    heading.style.fontWeight = "bold";
+    heading.style.color = "#9f476a";
+    heading.innerText = "Suggested Teas for you";
+    heading.setAttribute("class", "my-5 text-center");
+    heading.setAttribute("id", "help_heading");
+    suggested_tea.prepend(heading);
+  }
+
+  let count=0;
+  for (let k = 0; k < tea.length; k++) {
+    const ele = tea[k];
+    let teaClasses = ele.className.split(/\s+/).filter((ele) => ele !== "");
+    teaClasses = teaClasses.splice(6);
+    for (let i = 0; i < checkedArr.length; i++) {
+      const cl = checkedArr[i];
+      for (let j = 0; j < teaClasses.length; j++) {
+        if (cl === teaClasses[j]) {
+          ele.className = ele.className.replace("d-none", "");
+          count = count + 1;
+          if (count==4) {
+            return;
+          }
+        }
+      }
+    }
+  }
+}
+
+function remove_help() {
+  window.location.reload();
+}
+
+
+// Slider Code
+
 var index = 0;
 var i=0;
 var slider = document.getElementsByClassName("slider");
@@ -17,59 +74,5 @@ function auto(){
        index=1;
    }
    show(index);
-   setTimeout(auto,1000);
-}
-
-
-const submit_button = document.getElementById("submit-btn");
-const rest_button = document.getElementById("reset-btn");
-
-submit_button.addEventListener("click", displayTea);
-rest_button.addEventListener('click',remove_help);
-
-function displayTea() {
-  const checkbox = document.getElementsByClassName("form-check-input");
-  const checkedArr = [];
-  let j = 0;
-  for (let i = 0; i < checkbox.length; i++) {
-    if (checkbox[i].checked === true) {
-      checkedArr[j] = checkbox[i].value;
-      j++;
-    }
-  }
-  const tea = document.getElementsByClassName("tea");
-
-  const help_div = document.getElementById("help-div");
-
-  if (checkedArr.length !==0 && document.getElementById("help_heading")===null) {
-    const heading = document.createElement("h3");
-    heading.style.fontWeight = "bold";
-    heading.style.color = "#9f476a";
-    heading.innerText = "Suggested Teas for you";
-    heading.setAttribute("class", "my-5 text-center");
-    heading.setAttribute("id", "help_heading");
-    help_div.appendChild(heading);
-  }
-
-  let count=0;
-  for (let k = 0; k < tea.length; k++) {
-    const ele = tea[k];
-    let teaClasses = ele.className.split(/\s+/).filter((ele) => ele !== "");
-    for (let i = 0; i < checkedArr.length; i++) {
-      const cl = checkedArr[i];
-      for (let j = 0; j < teaClasses.length; j++) {
-        if (cl === teaClasses[j]) {
-          ele.className = ele.className.replace("d-none", "");
-          count = count + 1;
-          if (count==4) {
-            return;
-          }
-        }
-      }
-    }
-  }
-}
-
-function remove_help() {
-  window.location.reload();
+   setTimeout(auto,4000);
 }
