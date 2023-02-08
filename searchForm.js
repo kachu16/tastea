99980ca16f39a20e3568be69c99ad523eecdb200
Kajol Sharma <91527662+kachu16@users.noreply.github.com>
2023-02-08 16:26:32 +0530

    Delete searchForm.js

--- searchForm.js
+++
@@ -1,7 +0,0 @@
-const submitForm = () =>{
-    const text = document.getElementById('navbar_search').value;
-    localStorage.setItem("QUERY",text);
-
-}
-
-document.getElementById("search_form").addEventListener("submit", submitForm);
